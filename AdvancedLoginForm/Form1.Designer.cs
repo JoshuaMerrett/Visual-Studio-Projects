3415fbc1d47ca25a7b19dcbb85814c6eda1b3416
Shadow99hunter <45756040+Shadow99hunter@users.noreply.github.com>
2018-12-10 10:05:16 +0000

    Add files via upload

---
+++ AdvancedLoginForm/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace AdvancedLoginForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tcMainTabControl = new System.Windows.Forms.TabControl();
+            this.tpLogin = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLoginPassword = new System.Windows.Forms.TextBox();
+            this.txtLoginUsername = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.tpRegister = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtRegisterPassword = new System.Windows.Forms.TextBox();
+            this.txtRegisterUsername = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.tcMainTabControl.SuspendLayout();
+            this.tpLogin.SuspendLayout();
+            this.tpRegister.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tcMainTabControl
+            // 
+            this.tcMainTabControl.Controls.Add(this.tpLogin);
+            this.tcMainTabControl.Controls.Add(this.tpRegister);
+            this.tcMainTabControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tcMainTabControl.Location = new System.Drawing.Point(12, 12);
+            this.tcMainTabControl.Name = "tcMainTabControl";
+            this.tcMainTabControl.SelectedIndex = 0;
+            this.tcMainTabControl.Size = new System.Drawing.Size(446, 228);
+            this.tcMainTabControl.TabIndex = 0;
+            // 
+            // tpLogin
+            // 
+            this.tpLogin.Controls.Add(this.label2);
+            this.tpLogin.Controls.Add(this.label1);
+            this.tpLogin.Controls.Add(this.txtLoginPassword);
+            this.tpLogin.Controls.Add(this.txtLoginUsername);
+            this.tpLogin.Controls.Add(this.btnLogin);
+            this.tpLogin.Location = new System.Drawing.Point(4, 29);
+            this.tpLogin.Name = "tpLogin";
+            this.tpLogin.Padding = new System.Windows.Forms.Padding(3);
+            this.tpLogin.Size = new System.Drawing.Size(438, 195);
+            this.tpLogin.TabIndex = 0;
+            this.tpLogin.Text = "Login";
+            this.tpLogin.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username";
+            // 
+            // txtLoginPassword
+            // 
+            this.txtLoginPassword.Location = new System.Drawing.Point(118, 65);
+            this.txtLoginPassword.Name = "txtLoginPassword";
+            this.txtLoginPassword.PasswordChar = '*';
+            this.txtLoginPassword.Size = new System.Drawing.Size(131, 26);
+            this.txtLoginPassword.TabIndex = 2;
+            // 
+            // txtLoginUsername
+            // 
+            this.txtLoginUsername.Location = new System.Drawing.Point(118, 30);
+            this.txtLoginUsername.Name = "txtLoginUsername";
+            this.txtLoginUsername.Size = new System.Drawing.Size(131, 26);
+            this.txtLoginUsername.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(352, 158);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(80, 31);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // tpRegister
+            // 
+            this.tpRegister.Controls.Add(this.label3);
+            this.tpRegister.Controls.Add(this.label4);
+            this.tpRegister.Controls.Add(this.txtRegisterPassword);
+            this.tpRegister.Controls.Add(this.txtRegisterUsername);
+            this.tpRegister.Controls.Add(this.btnRegister);
+            this.tpRegister.Location = new System.Drawing.Point(4, 29);
+            this.tpRegister.Name = "tpRegister";
+            this.tpRegister.Padding = new System.Windows.Forms.Padding(3);
+            this.tpRegister.Size = new System.Drawing.Size(438, 195);
+            this.tpRegister.TabIndex = 1;
+            this.tpRegister.Text = "Register";
+            this.tpRegister.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Username";
+            // 
+            // txtRegisterPassword
+            // 
+            this.txtRegisterPassword.Location = new System.Drawing.Point(118, 65);
+            this.txtRegisterPassword.Name = "txtRegisterPassword";
+            this.txtRegisterPassword.PasswordChar = '*';
+            this.txtRegisterPassword.Size = new System.Drawing.Size(131, 26);
+            this.txtRegisterPassword.TabIndex = 6;
+            // 
+            // txtRegisterUsername
+            // 
+            this.txtRegisterUsername.Location = new System.Drawing.Point(118, 30);
+            this.txtRegisterUsername.Name = "txtRegisterUsername";
+            this.txtRegisterUsername.Size = new System.Drawing.Size(131, 26);
+            this.txtRegisterUsername.TabIndex = 5;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(352, 158);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(80, 31);
+            this.btnRegister.TabIndex = 1;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 250);
+            this.Controls.Add(this.tcMainTabControl);
+            this.Name = "Form1";
+            this.Text = "Advanced Login";
+            this.tcMainTabControl.ResumeLayout(false);
+            this.tpLogin.ResumeLayout(false);
+            this.tpLogin.PerformLayout();
+            this.tpRegister.ResumeLayout(false);
+            this.tpRegister.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tcMainTabControl;
+        private System.Windows.Forms.TabPage tpLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLoginPassword;
+        private System.Windows.Forms.TextBox txtLoginUsername;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TabPage tpRegister;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtRegisterPassword;
+        private System.Windows.Forms.TextBox txtRegisterUsername;
+        private System.Windows.Forms.Button btnRegister;
+    }
+}
+
---
+++ AdvancedLoginForm/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedLoginForm
+{
+    public partial class Form1 : Form
+    {
+        //Lists to store registered usernames and passwords
+        List<string> usernamesList = new List<string>();
+        List<string> passwordsList = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtLoginPassword.UseSystemPasswordChar = true;
+            txtRegisterPassword.UseSystemPasswordChar = true;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+            int usernameIndex = 0;
+            int passwordIndex = 0;
+            bool usernameFound = false;
+            bool passwordFound = false;
+
+            for (int i = 0; i < usernamesList.Count; i++)
+            {
+                if (usernamesList[i] == txtLoginUsername.Text)
+                {
+                    usernameIndex = i;
+                    usernameFound = true;
+                }
+            }
+
+            for (int i = 0; i < passwordsList.Count; i++)
+            {
+                if (passwordsList[i] == txtLoginPassword.Text)
+                {
+                    passwordIndex = i;
+                    passwordFound = true;
+                }
+            }
+
+            if (usernameFound == true && passwordFound == true)
+            {
+                if (usernameIndex == passwordIndex)
+                {
+                    MessageBox.Show("Login successful");
+                    Form2 form2 = new Form2();
+                    this.Hide();
+                    form2.Show();
+                }
+
+                else
+                {
+                    MessageBox.Show("Incorrect Login Details");
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Incorrect Login Details");
+            }
+
+            txtLoginUsername.Clear();
+            txtLoginPassword.Clear();
+
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtRegisterUsername.Text) || string.IsNullOrWhiteSpace(txtRegisterPassword.Text))
+            {
+                MessageBox.Show("Complete all fields");
+            }
+
+            else
+            {
+                usernamesList.Add(txtRegisterUsername.Text);
+                passwordsList.Add(txtRegisterPassword.Text);
+                MessageBox.Show("Registration Successful");
+                tcMainTabControl.SelectedTab = tpLogin;
+            }
+        }
+    }
+}

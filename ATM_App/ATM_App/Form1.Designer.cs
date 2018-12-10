e9c322229be8c80cc0f0a6bb31e16c36ccd825db
Shadow99hunter <45756040+Shadow99hunter@users.noreply.github.com>
2018-12-10 10:09:49 +0000

    Add files via upload

---
+++ ATM_App/ATM_App/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace ATM_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.btnDeposit = new System.Windows.Forms.Button();
+            this.btnWithdraw = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textbox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pbAmount5 = new System.Windows.Forms.ProgressBar();
+            this.pbAmount20 = new System.Windows.Forms.ProgressBar();
+            this.pbAmount10 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBalance = new System.Windows.Forms.TextBox();
+            this.lbl20Left = new System.Windows.Forms.Label();
+            this.lbl10Left = new System.Windows.Forms.Label();
+            this.lbl5Left = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(24, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Amount £";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAmount.Location = new System.Drawing.Point(133, 34);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(131, 31);
+            this.txtAmount.TabIndex = 1;
+            // 
+            // btnDeposit
+            // 
+            this.btnDeposit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeposit.Location = new System.Drawing.Point(133, 87);
+            this.btnDeposit.Name = "btnDeposit";
+            this.btnDeposit.Size = new System.Drawing.Size(131, 35);
+            this.btnDeposit.TabIndex = 2;
+            this.btnDeposit.Text = "Deposit";
+            this.btnDeposit.UseVisualStyleBackColor = true;
+            this.btnDeposit.Click += new System.EventHandler(this.btnDeposit_Click);
+            // 
+            // btnWithdraw
+            // 
+            this.btnWithdraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWithdraw.Location = new System.Drawing.Point(133, 149);
+            this.btnWithdraw.Name = "btnWithdraw";
+            this.btnWithdraw.Size = new System.Drawing.Size(131, 35);
+            this.btnWithdraw.TabIndex = 3;
+            this.btnWithdraw.Text = "Withdraw";
+            this.btnWithdraw.UseVisualStyleBackColor = true;
+            this.btnWithdraw.Click += new System.EventHandler(this.btnWithdraw_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(424, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Balance";
+            // 
+            // textbox
+            // 
+            this.textbox.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textbox.Location = new System.Drawing.Point(385, 68);
+            this.textbox.Name = "textbox";
+            this.textbox.ReadOnly = true;
+            this.textbox.Size = new System.Drawing.Size(174, 116);
+            this.textbox.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(602, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(184, 147);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pbAmount5
+            // 
+            this.pbAmount5.Location = new System.Drawing.Point(133, 217);
+            this.pbAmount5.Name = "pbAmount5";
+            this.pbAmount5.Size = new System.Drawing.Size(426, 23);
+            this.pbAmount5.TabIndex = 7;
+            // 
+            // pbAmount20
+            // 
+            this.pbAmount20.Location = new System.Drawing.Point(133, 299);
+            this.pbAmount20.Name = "pbAmount20";
+            this.pbAmount20.Size = new System.Drawing.Size(426, 23);
+            this.pbAmount20.TabIndex = 8;
+            // 
+            // pbAmount10
+            // 
+            this.pbAmount10.Location = new System.Drawing.Point(133, 257);
+            this.pbAmount10.Name = "pbAmount10";
+            this.pbAmount10.Size = new System.Drawing.Size(426, 23);
+            this.pbAmount10.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(100, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "£5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(91, 302);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "£20";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(91, 260);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "£10";
+            // 
+            // txtBalance
+            // 
+            this.txtBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBalance.Location = new System.Drawing.Point(385, 87);
+            this.txtBalance.Name = "txtBalance";
+            this.txtBalance.ReadOnly = true;
+            this.txtBalance.Size = new System.Drawing.Size(174, 83);
+            this.txtBalance.TabIndex = 13;
+            // 
+            // lbl20Left
+            // 
+            this.lbl20Left.AutoSize = true;
+            this.lbl20Left.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl20Left.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbl20Left.Location = new System.Drawing.Point(565, 302);
+            this.lbl20Left.Name = "lbl20Left";
+            this.lbl20Left.Size = new System.Drawing.Size(48, 20);
+            this.lbl20Left.TabIndex = 14;
+            this.lbl20Left.Text = "Label";
+            // 
+            // lbl10Left
+            // 
+            this.lbl10Left.AutoSize = true;
+            this.lbl10Left.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl10Left.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbl10Left.Location = new System.Drawing.Point(565, 260);
+            this.lbl10Left.Name = "lbl10Left";
+            this.lbl10Left.Size = new System.Drawing.Size(48, 20);
+            this.lbl10Left.TabIndex = 15;
+            this.lbl10Left.Text = "Label";
+            // 
+            // lbl5Left
+            // 
+            this.lbl5Left.AutoSize = true;
+            this.lbl5Left.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl5Left.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbl5Left.Location = new System.Drawing.Point(565, 220);
+            this.lbl5Left.Name = "lbl5Left";
+            this.lbl5Left.Size = new System.Drawing.Size(48, 20);
+            this.lbl5Left.TabIndex = 16;
+            this.lbl5Left.Text = "Label";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(809, 340);
+            this.Controls.Add(this.lbl5Left);
+            this.Controls.Add(this.lbl10Left);
+            this.Controls.Add(this.lbl20Left);
+            this.Controls.Add(this.txtBalance);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pbAmount10);
+            this.Controls.Add(this.pbAmount20);
+            this.Controls.Add(this.pbAmount5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnWithdraw);
+            this.Controls.Add(this.btnDeposit);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ATM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Button btnDeposit;
+        private System.Windows.Forms.Button btnWithdraw;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textbox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar pbAmount5;
+        private System.Windows.Forms.ProgressBar pbAmount20;
+        private System.Windows.Forms.ProgressBar pbAmount10;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBalance;
+        private System.Windows.Forms.Label lbl20Left;
+        private System.Windows.Forms.Label lbl10Left;
+        private System.Windows.Forms.Label lbl5Left;
+    }
+}
+
---
+++ ATM_App/ATM_App/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATM_App
+{
+    public partial class Form1 : Form
+    {
+
+        double balance = 0, amount = 0;
+        int amount5Left = 0, amount10Left = 0, amount20Left =  0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtBalance.Text = "0";
+
+            lbl5Left.Text = amount5Left.ToString() + " / " + "300";
+            lbl10Left.Text = amount10Left.ToString() + " / " + "400";
+            lbl20Left.Text = amount20Left.ToString() + " / " + "500";
+
+            pbAmount5.Maximum = 300;
+            pbAmount10.Maximum = 400;
+            pbAmount20.Maximum = 500;
+            pbAmount5.Value = amount5Left;
+            pbAmount10.Value = amount10Left;
+            pbAmount20.Value = amount20Left;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDeposit_Click(object sender, EventArgs e)
+        {
+
+            bool correctParsed = false;
+            try
+            {
+                amount = Convert.ToInt32(txtAmount.Text);
+                correctParsed = true;
+            }
+            catch
+            {
+                MessageBox.Show("Amount to be deposited could not be parsed! Only enter numbers");
+            }
+
+            if (correctParsed == true)
+            {
+                if ((amount % 5) == 0)
+                {
+                    balance = balance + amount;
+                    txtBalance.Text = balance.ToString();
+
+                    // multiples of 20
+                    if (amount >= 20)
+                    {
+                        for (int i = 0; amount >= 20 && amount20Left <= pbAmount20.Maximum; i++)
+                        {
+                            amount -= 20;
+                            amount20Left++;
+                            pbAmount20.Value = amount20Left;
+                            lbl20Left.Text = amount20Left.ToString() + " / " + "500";
+                        }
+                    }
+
+                    if (amount >= 10)
+                    {
+                        for (int i = 0; amount >= 10 && amount10Left <= pbAmount10.Maximum; i++)
+                        {
+                            amount -= 10;
+                            amount10Left++;
+                            pbAmount10.Value = amount10Left;
+                            lbl10Left.Text = amount10Left.ToString() + " / " + "400";
+                        }
+                    }
+
+                    if (amount >= 5)
+                    {
+                        for (int i = 0; amount >= 5 && amount5Left <= pbAmount5.Maximum; i++)
+                        {
+                            amount -= 5;
+                            amount5Left++;
+                            pbAmount5.Value = amount5Left;
+                            lbl5Left.Text = amount5Left.ToString() + " / " + "300";
+                        }
+                    }
+                }
+
+                else
+                {
+                    MessageBox.Show("Please enter a multiple of 5!");
+                }
+            }
+            else if (amount > balance)
+            {
+                MessageBox.Show("You do not have enough in your account to withdraw: £" + amount.ToString());
+            }
+        }
+
+        private void btnWithdraw_Click(object sender, EventArgs e)
+        {
+
+            bool correctParsed = false;
+            try
+            {
+                amount = Convert.ToInt32(txtAmount.Text);
+                correctParsed = true;
+            }
+            catch
+            {
+                MessageBox.Show("Amount to be deposited could not be parsed! Only enter numbers");
+            }
+
+            if (correctParsed == true)
+            {
+                if (amount <= balance)
+                {
+                    if ((amount % 5) == 0)
+                    {
+                        balance = balance - amount;
+                        txtBalance.Text = balance.ToString();
+
+                        // multiples of 20
+                        if (amount >= 20)
+                        {
+                            for (int i = 0; amount >= 20; i++)
+                            {
+                                amount -= 20;
+                                amount20Left--;
+                                pbAmount20.Value = amount20Left;
+                                lbl20Left.Text = amount20Left.ToString() + " / " + "500";
+                            }
+                        }
+                        if (amount >= 10)
+                        {
+                            for (int i = 0; amount >= 10; i++)
+                            {
+                                amount -= 10;
+                                amount10Left--;
+                                pbAmount10.Value = amount10Left;
+                                lbl10Left.Text = amount10Left.ToString() + " / " + "400";
+                            }
+                        }
+                        if (amount >= 5)
+                        {
+                            for (int i = 0; amount >= 5; i++)
+                            {
+                                amount -= 5;
+                                amount5Left--;
+                                pbAmount5.Value = amount5Left;
+                                lbl5Left.Text = amount5Left.ToString() + " / " + "300";
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter a multiple of 5!");
+                    }
+                }
+                else if (amount > balance)
+                {
+                    MessageBox.Show("You do not have enough in your account to withdraw: £" + amount.ToString());
+                }
+            }
+
+        }
+    }
+}

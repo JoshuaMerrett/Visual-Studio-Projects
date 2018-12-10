b95a7f849a3cd9c2d8354c31d0cc07568d64600a
Shadow99hunter <45756040+Shadow99hunter@users.noreply.github.com>
2018-12-10 10:08:34 +0000

    Add files via upload

---
+++ AssignmentApp/AssignmentApp/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace AssignmentApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstItem = new System.Windows.Forms.ListBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblDeveloper = new System.Windows.Forms.Label();
+            this.txtDeveloper = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRelease = new System.Windows.Forms.TextBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lstBasket = new System.Windows.Forms.ListBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnCheckout = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstItem
+            // 
+            this.lstItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstItem.FormattingEnabled = true;
+            this.lstItem.ItemHeight = 20;
+            this.lstItem.Location = new System.Drawing.Point(39, 37);
+            this.lstItem.Name = "lstItem";
+            this.lstItem.Size = new System.Drawing.Size(215, 304);
+            this.lstItem.TabIndex = 0;
+            this.lstItem.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lstItem_MouseClick);
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(385, 37);
+            this.txtName.Name = "txtName";
+            this.txtName.ReadOnly = true;
+            this.txtName.Size = new System.Drawing.Size(169, 22);
+            this.txtName.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(271, 40);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(51, 20);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name";
+            // 
+            // lblDeveloper
+            // 
+            this.lblDeveloper.AutoSize = true;
+            this.lblDeveloper.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDeveloper.Location = new System.Drawing.Point(271, 84);
+            this.lblDeveloper.Name = "lblDeveloper";
+            this.lblDeveloper.Size = new System.Drawing.Size(81, 20);
+            this.lblDeveloper.TabIndex = 3;
+            this.lblDeveloper.Text = "Developer";
+            // 
+            // txtDeveloper
+            // 
+            this.txtDeveloper.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDeveloper.Location = new System.Drawing.Point(385, 81);
+            this.txtDeveloper.Name = "txtDeveloper";
+            this.txtDeveloper.ReadOnly = true;
+            this.txtDeveloper.Size = new System.Drawing.Size(169, 22);
+            this.txtDeveloper.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(271, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Release Date";
+            // 
+            // txtRelease
+            // 
+            this.txtRelease.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRelease.Location = new System.Drawing.Point(385, 121);
+            this.txtRelease.Name = "txtRelease";
+            this.txtRelease.ReadOnly = true;
+            this.txtRelease.Size = new System.Drawing.Size(169, 22);
+            this.txtRelease.TabIndex = 6;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescription.Location = new System.Drawing.Point(271, 166);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(89, 20);
+            this.lblDescription.TabIndex = 7;
+            this.lblDescription.Text = "Description";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescription.Location = new System.Drawing.Point(385, 168);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.ReadOnly = true;
+            this.txtDescription.Size = new System.Drawing.Size(169, 104);
+            this.txtDescription.TabIndex = 8;
+            this.txtDescription.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(271, 287);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Price";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrice.Location = new System.Drawing.Point(385, 284);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.ReadOnly = true;
+            this.txtPrice.Size = new System.Drawing.Size(169, 22);
+            this.txtPrice.TabIndex = 10;
+            // 
+            // numericUpDown
+            // 
+            this.numericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown.Location = new System.Drawing.Point(385, 319);
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(169, 26);
+            this.numericUpDown.TabIndex = 11;
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuantity.Location = new System.Drawing.Point(271, 321);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(68, 20);
+            this.lblQuantity.TabIndex = 12;
+            this.lblQuantity.Text = "Quantity";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(385, 360);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(169, 31);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "Add to Basket";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lstBasket
+            // 
+            this.lstBasket.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstBasket.FormattingEnabled = true;
+            this.lstBasket.ItemHeight = 20;
+            this.lstBasket.Location = new System.Drawing.Point(560, 37);
+            this.lstBasket.Name = "lstBasket";
+            this.lstBasket.Size = new System.Drawing.Size(215, 304);
+            this.lstBasket.TabIndex = 14;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(560, 360);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(44, 20);
+            this.lblTotal.TabIndex = 15;
+            this.lblTotal.Text = "Total";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotal.Location = new System.Drawing.Point(620, 357);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(154, 26);
+            this.txtTotal.TabIndex = 16;
+            // 
+            // btnCheckout
+            // 
+            this.btnCheckout.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheckout.Location = new System.Drawing.Point(620, 389);
+            this.btnCheckout.Name = "btnCheckout";
+            this.btnCheckout.Size = new System.Drawing.Size(154, 31);
+            this.btnCheckout.TabIndex = 17;
+            this.btnCheckout.Text = "Checkout";
+            this.btnCheckout.UseVisualStyleBackColor = true;
+            this.btnCheckout.Click += new System.EventHandler(this.btnCheckout_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 432);
+            this.Controls.Add(this.btnCheckout);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lstBasket);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.numericUpDown);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.txtRelease);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDeveloper);
+            this.Controls.Add(this.lblDeveloper);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lstItem);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstItem;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblDeveloper;
+        private System.Windows.Forms.TextBox txtDeveloper;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRelease;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.RichTextBox txtDescription;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.NumericUpDown numericUpDown;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lstBasket;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button btnCheckout;
+    }
+}
+
---
+++ AssignmentApp/AssignmentApp/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AssignmentApp
+{
+    public partial class Form1 : Form
+    {
+        double total = 0;
+        List<Game> games = new List<Game>();
+        List<Game> checkout = new List<Game>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Game game1 = new Game();
+            game1.name = "Assassin's Creed Odyssey";
+            game1.developer = "Ubisoft";
+            game1.releaseDate = "05/10/2018";
+            game1.description = "Assassin's Creed Odyssey places more emphasis on role-playing elements than previous games in the series. The game contains dialogue options, branching quests, and multiple endings.";
+            game1.price = 49.99;
+            game1.quantity = 1;
+            games.Add(game1);
+
+            Game game2 = new Game();
+            game2.name = "Call Of Duty: Black Ops 4";
+            game2.developer = "Treyarch";
+            game2.releaseDate = "12/10/2018";
+            game2.description = "Call of Duty: Black Ops 4 is a multiplayer first-person shooter video game. Unlike previous titles in the Call of Duty series, Black Ops 4 is the first entry to not feature a traditional single-player campaign, and contains only Multiplayer, Zombies and a new battle royale mode called Blackout.";
+            game2.price = 49.99;
+            game2.quantity = 1;
+            games.Add(game2);
+
+            Game game3 = new Game();
+            game3.name = "Dark Souls Remastered";
+            game3.developer = "From Software";
+            game3.releaseDate = "23/05/2018";
+            game3.description = "Dark Souls takes place in the fictional kingdom of Lordran, where players assume the role of a cursed undead character who begins a pilgrimage to discover the fate of their kind.";
+            game3.price = 29.99;
+            game3.quantity = 1;
+            games.Add(game3);
+
+            Game game4 = new Game();
+            game4.name = "Red Dead Redemption 2";
+            game4.developer = "Rockstar";
+            game4.releaseDate = "26/10/2018";
+            game4.description = "Red Dead Redemption 2 is a Western-themed action-adventure game set in an open world environment. It is played from either a third or first-person perspective and the player controls Arthur Morgan, an outlaw and a member of the Van der Linde gang.";
+            game4.price = 49.99;
+            game4.quantity = 1;
+            games.Add(game4);
+
+            DisplayGames();
+        }
+
+        private void lstItem_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (lstItem.SelectedIndex > -1)
+            {
+                int i = lstItem.SelectedIndex;
+
+                txtName.Text = games[i].name;
+                txtDeveloper.Text = games[i].developer;
+                txtRelease.Text = games[i].releaseDate;
+                txtDescription.Text = games[i].description;
+                txtPrice.Text = games[i].price.ToString();
+                numericUpDown.Value = games[i].quantity;
+            }
+        }
+
+        private void DisplayGames()
+        {
+            for (int i = 0; i < games.Count; i++)
+            {
+                lstItem.Items.Add(games[i].name);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Game newGame = new Game();
+            newGame.name = txtName.Text;
+            newGame.developer = null;
+            newGame.releaseDate = null;
+            newGame.description = null;
+            newGame.price = Convert.ToDouble(txtPrice.Text);
+            newGame.quantity = Convert.ToInt32(numericUpDown.Value);
+
+            checkout.Add(newGame);
+
+            for(int i = 0; i < checkout.Count; i++)
+            {
+                for (int n = 0; n < checkout.Count; n++)
+                {
+                    if (checkout[i].name == checkout[n].name && i != n)
+                    {
+                        checkout.RemoveAt(n);
+                        checkout[i].quantity = checkout[i].quantity + newGame.quantity;
+                    }
+                }
+            }
+
+            DisplayBasket();
+
+            double gamePrice = newGame.price * newGame.quantity;
+            total = total + gamePrice;
+
+            txtTotal.Text = Convert.ToString(total);
+        }
+
+        private void btnCheckout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Order placed: " + "£" + total);
+
+            txtName.Clear();
+            txtDeveloper.Clear();
+            txtRelease.Clear();
+            txtDescription.Clear();
+            txtPrice.Clear();
+            txtTotal.Clear();
+
+            lstBasket.Items.Clear();
+        }
+        private void DisplayBasket()
+        {
+            lstBasket.Items.Clear();
+
+            for (int i = 0; i < checkout.Count; i++)
+            {
+                lstBasket.Items.Add(checkout[i].name + " [" + checkout[i].quantity.ToString() + "]");
+            }
+        }
+    }
+    public class Game
+    {
+        public string name;
+        public string developer;
+        public string releaseDate;
+        public string description;
+        public double price;
+        public int quantity;
+    }
+}
